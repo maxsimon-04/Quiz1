a30b348dd99dbfdb6f68735ee0e058ef06a53adf
maxsimon-04 <maximilian@postfach-24.de>
2022-11-26 03:30:25 +0100

    Quiz1

--- app/src/main/java/com/example/quiz1/MainActivity.kt
+++ app/src/main/java/com/example/quiz1/MainActivity.kt
@@ -14,6 +14,8 @@ class MainActivity : GameActivity() {
         super.onWindowFocusChanged(hasFocus)
         if (hasFocus) {
             hideSystemUi()
+        }else{
+            println("Hallo")
         }
     }
 
